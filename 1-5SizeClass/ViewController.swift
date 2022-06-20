6955e64b9943c125a48d0694083d428ded1fd000
Daniel Mateos <luisdmateos@outlook.com>
2022-06-20 09:45:12 -0500

    Initial Commit

---
+++ 1-5SizeClass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1-5SizeClass
+//
+//  Created by djdenielb on 20/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
